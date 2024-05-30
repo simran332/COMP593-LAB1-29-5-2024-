53d1e5957887e0eb193a4c57721a9ff711c40f94
simran332 <skaur332@flemingcollege.ca>
2024-05-29 21:22:46 -0400

    Added comment to script1.py

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
+# Print lines from Zen of Python
 print("There should be one-- and preferably only one --obvious way to do it.")
 print("Althought that way may not br obvious at first unless you're Dutch.")
